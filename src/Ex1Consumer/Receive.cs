5bba4724a5523ea61a01eb7bf064ea5dbdb42b31
Angelito Casagrande <angelito@angelito.com.br>
2020-05-30 12:08:03 -0300

    Add example 1

---
+++ src/Ex1Consumer/Receive.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Ex1Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "docker.local" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                while (true)
+                {
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine($"[x] Received: {message}");
+                    };
+                    channel.BasicConsume(queue: "hello",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
---
+++ src/Ex1Publisher/Send.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Ex1Publisher
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "docker.local" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var count = 0;
+                while (true)
+                {
+                    string message = $"Hello World! {count++}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine($"[x] Sent: {message}");
+
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
