1822d6cb97dd7f05a343c576385ce637d0296cca
Angelito Casagrande <angelito@angelito.com.br>
2020-05-30 14:29:38 -0300

    Add Example 3

---
+++ src/Ex3Consumer/Receive.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Ex3Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "docker.local" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.BasicQos(0, prefetchCount: 2, false);
+
+                var queueName1 = "imageProcess";
+                var queueName2 = "imageArchive";
+
+                channel.QueueDeclare(queue: queueName1, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: queueName2, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer1 = new EventingBasicConsumer(channel);
+                var consumer2 = new EventingBasicConsumer(channel);
+
+                consumer1.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine($"[x] C1 - Process: {message}");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch
+                    {
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                    System.Threading.Thread.Sleep(50);
+                };
+
+                consumer2.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine($"[x] C2 - Archive: {message}");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch
+                    {
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                    System.Threading.Thread.Sleep(50);
+                };
+
+                channel.BasicConsume(queue: queueName1, autoAck: false, consumer: consumer1);
+                channel.BasicConsume(queue: queueName2, autoAck: false, consumer: consumer2);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ src/Ex3Publisher/Send.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Ex3Publisher
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "docker.local" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "imageProcess", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: "imageArchive", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                channel.ExchangeDeclare(exchange: "image", type: "direct");
+
+                channel.QueueBind(queue: "imageProcess", exchange: "image", routingKey: "crop");
+                channel.QueueBind(queue: "imageProcess", exchange: "image", routingKey: "resize");
+                channel.QueueBind(queue: "imageArchive", exchange: "image", routingKey: "resize");
+
+                var count = 0;
+
+                while (true)
+                {
+                    string message = $"Image process message! {count++}";
+
+                    var body1 = Encoding.UTF8.GetBytes($"{message} - crop");
+                    var body2 = Encoding.UTF8.GetBytes($"{message} - resize");
+
+                    channel.BasicPublish(exchange: "image", routingKey: "crop", basicProperties: null, body: body1);
+                    channel.BasicPublish(exchange: "image", routingKey: "resize", basicProperties: null, body: body2);
+
+                    Console.WriteLine($"[x] Sent: {message}");
+
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
