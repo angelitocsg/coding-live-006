8831fd191e927b876a91894abdaccac1a0352516
Angelito Casagrande <angelito@angelito.com.br>
2020-05-30 15:29:35 -0300

    Add Example 4

---
+++ src/Ex4Consumer/Receive.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Ex4Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "docker.local" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.BasicQos(0, prefetchCount: 3, true);
+
+                channel.QueueDeclare(queue: "email", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: "whatsapp", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: "external", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer1 = new EventingBasicConsumer(channel);
+                var consumer2 = new EventingBasicConsumer(channel);
+                var consumer3 = new EventingBasicConsumer(channel);
+
+                consumer1.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine($"[x] Send Email: {message}");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch
+                    {
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                    System.Threading.Thread.Sleep(200);
+                };
+
+                consumer2.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine($"[x] Send WhatsApp: {message}");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch
+                    {
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                    System.Threading.Thread.Sleep(200);
+                };
+
+                consumer3.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine($"[x] Send External: {message}");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch
+                    {
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                    System.Threading.Thread.Sleep(200);
+                };
+
+                channel.BasicConsume(queue: "email", autoAck: false, consumer: consumer1);
+                channel.BasicConsume(queue: "whatsapp", autoAck: false, consumer: consumer2);
+                channel.BasicConsume(queue: "external", autoAck: false, consumer: consumer3);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ src/Ex4Publisher/Send.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Ex4Publisher
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "docker.local" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "email", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: "whatsapp", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: "external", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                channel.ExchangeDeclare(exchange: "alerts", type: "topic");
+
+                channel.QueueBind(queue: "email", exchange: "alerts", routingKey: "*.web.*");
+                channel.QueueBind(queue: "whatsapp", exchange: "alerts", routingKey: "*.mobile.*");
+                channel.QueueBind(queue: "external", exchange: "alerts", routingKey: "game.#");
+
+                var count = 0;
+
+                while (true)
+                {
+                    string message = $"Alert message! {count++}";
+
+                    var body1 = Encoding.UTF8.GetBytes($"{message} - payments");
+                    var body2 = Encoding.UTF8.GetBytes($"{message} - orders");
+                    var body3 = Encoding.UTF8.GetBytes($"{message} - clients");
+                    var body4 = Encoding.UTF8.GetBytes($"{message} - game");
+
+                    channel.BasicPublish(exchange: "alerts", routingKey: "payments.web.exec", basicProperties: null, body: body1);
+                    channel.BasicPublish(exchange: "alerts", routingKey: "orders.web.exec", basicProperties: null, body: body2);
+                    channel.BasicPublish(exchange: "alerts", routingKey: "clients.web.exec", basicProperties: null, body: body3);
+
+                    channel.BasicPublish(exchange: "alerts", routingKey: "game.mobile.playing", basicProperties: null, body: body4);
+
+                    Console.WriteLine($"[x] Sent: {message}");
+
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
