ddc2fbb93dcce7511413f5d05734e94234f1fcef
Angelito Casagrande <angelito@angelito.com.br>
2020-05-30 14:07:54 -0300

    Add Example2 - Fanout

---
+++ src/Ex2Consumer/Receive.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Ex2Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "docker.local" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var queueName1 = "myQueue1";
+                var queueName2 = "myQueue2";
+
+                channel.QueueDeclare(queue: queueName1, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: queueName2, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer1 = new EventingBasicConsumer(channel);
+                var consumer2 = new EventingBasicConsumer(channel);
+
+                consumer1.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine($"[x] C1 - Received: {message}");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch
+                    {
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                    System.Threading.Thread.Sleep(50);
+                };
+
+                consumer2.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine($"[x] C2 - Received: {message}");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch
+                    {
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                    System.Threading.Thread.Sleep(50);
+                };
+
+                channel.BasicConsume(queue: queueName1, autoAck: false, consumer: consumer1);
+                channel.BasicConsume(queue: queueName2, autoAck: false, consumer: consumer2);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ src/Ex2Publisher/Send.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Ex2Publisher
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "docker.local" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "myQueue1", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: "myQueue2", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                channel.QueueBind(queue: "myQueue1", exchange: "logs", "");
+                channel.QueueBind(queue: "myQueue2", exchange: "logs", "");
+
+                var count = 0;
+
+                while (true)
+                {
+                    string message = $"Fanout message! {count++}";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+
+                    Console.WriteLine($"[x] Sent: {message}");
+
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
